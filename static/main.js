65aca9891ba6f92b285a6bd8f2fe9df5afb0bb69
Beniamin <benek9311@wp.pl>
2025-08-08 13:16:31 +0200

    Dodanie funkcji ładujące oraz renderującej kanały oraz funkcje pomocnicze formatujące numery

--- static/main.js
+++ static/main.js
@@ -9,4 +9,58 @@ let sortDirection = "asc"; // lub "desc"
 
 document.addEventListener("DOMContentLoaded", init);
 
-function init() {}
+async function init() {
+  await loadChannels();
+}
+
+async function loadChannels() {
+  try {
+    const res = await fetch("./channels.json");
+    if (!res.ok) throw new Error(`Status code ${res.status}`);
+    const data = await res.json();
+
+    channelsData = data.map(channel => ({
+      title: channel.title,
+      logo: channel.thumbnails.high.url,
+      url: channel.customUrl,
+      subscribers: parseNumber(channel.statistics.subscriberCount),
+      videos: parseNumber(channel.statistics.videoCount),
+      views: parseNumber(channel.statistics.viewCount),
+    }));
+
+    renderChannels(channelsData);
+  } catch (err) {
+    console.error("Błąd podczas pobierania kanałów:", err);
+    contentWrapper.innerHTML = "<p>Nie udało się wczytać kanałów.</p>";
+  }
+}
+
+function renderChannels(channels) {
+  contentWrapper.innerHTML = channels
+    .map(
+      channel => `
+    <article class="channel" data-url="${channel.url}">
+      <img class="channel__logo" src="${channel.logo}" alt="${channel.title}">
+      <h2 class="channel__title">${channel.title}</h2>
+      <p class="channel__stats">
+        SUBSCRIBERS: ${formatNumber(channel.subscribers)}<br>
+        VIDEOS: ${formatNumber(channel.videos)}<br>
+        VIEWS: ${formatNumber(channel.views)}
+      </p>
+    </article>
+  `
+    )
+    .join("");
+}
+
+function parseNumber(value) {
+  if (typeof value === "number") return value;
+
+  return Number(
+    String(value).replace(/\s/g, "").replace(/\./g, "").replace(/,/g, "")
+  );
+}
+
+function formatNumber(n) {
+  return n.toLocaleString("en-US");
+}
